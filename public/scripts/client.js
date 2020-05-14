606052873a2f9e427da43a197d543b0bacbe34a0
YoonSoon Kang <kangyoonsoon@hotmail.com>
2020-05-13 22:54:20 -0400

    Finished Fetching tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,43 +5,43 @@
  */
 
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac"
-    },
-    content: {
-      text:
-        "If I have seen further it is by standing on the shoulders of giants"
-    },
-    created_at: 1461116232227
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd"
-    },
-    content: {
-      text: "Je pense , donc je suis"
-    },
-    created_at: 1461113959088
-  }
-];
+// const tweetData = [
+//   {
+//     user: {
+//       name: "Newton",
+//       avatars: "https://i.imgur.com/73hZDYK.png",
+//       handle: "@SirIsaac"
+//     },
+//     content: {
+//       text:
+//         "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     created_at: 1461116232227
+//   },
+//   {
+//     user: {
+//       name: "Descartes",
+//       avatars: "https://i.imgur.com/nlhLi3I.png",
+//       handle: "@rd"
+//     },
+//     content: {
+//       text: "Je pense , donc je suis"
+//     },
+//     created_at: 1461113959088
+//   }
+// ];
 
-const oneTweet = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac"
-  },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants"
-  },
-  created_at: 1461116232227
-};
+// const oneTweet = {
+//   user: {
+//     name: "Newton",
+//     avatars: "https://i.imgur.com/73hZDYK.png",
+//     handle: "@SirIsaac"
+//   },
+//   content: {
+//     text: "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   created_at: 1461116232227
+// };
 
 const renderTweets = function(tweets) {
   // loops through tweets
@@ -63,7 +63,7 @@ const createTweetElement = tw => {
       <span class="head4" id="header-name">${tw["user"].handle}</span>
     </div>
     <div class="tweet-container-content">
-  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      ${tw["content"].text}
     </div>
     <footer>
       <span>10 days ago</span>
@@ -72,12 +72,70 @@ const createTweetElement = tw => {
   return $tweet;
 };
 
-// console.log($tweet);
+// ajax load tweets
+const loadTweets = () => {
+  $.ajax({
+    url: "http://localhost:8080/tweets",
+    type: "get",
+    dataType: "json",
+    success: function(data) {
+      console.log("success: ", data);
+      renderTweets(data);
+    }
+  });
+};
 
-const test = createTweetElement(oneTweet);
+const containerEffect = () => {
+  $(".tweet-container").mouseover(function() {
+    console.log(this);
+    $(this)
+      .find(".tweet-container-content")
+      .css("color", "#222");
+    $(this)
+      .find("span")
+      .css("color", "#222");
+    $(this)
+      .find("#header-name")
+      // .text("@Sirlsaac")
+      .css({ opacity: 1 })
+      .css("color", "#666")
+      .css("font-size", "2rem");
+  });
+  $(".tweet-container").mouseleave(function() {
+    console.log(this);
+    $(this)
+      .find("#header-name")
+      // .css("");
+      .css({ opacity: 0 });
+    $(this)
+      .find(".tweet-container-content")
+      .css("color", "#aaa");
+    $(this)
+      .find("span")
+      .css("color", "#aaa");
+  });
+};
 
 $(document).ready(function() {
-  // $forRenderContainer = renderTweets(data);
-  // $(".tweets-container").append($forRenderContainer);
-  renderTweets(data);
+  // sending data to server by ajax
+  $("#formSubmit").on("submit", function(event) {
+    event.preventDefault();
+
+    $.ajax({
+      url: "http://localhost:8080/tweets",
+      type: "post",
+      data: $(this).serialize(),
+      success: function(data) {
+        console.log("success: ", data);
+        // loadTweets();
+      }
+    });
+    //initialize tweet-container
+    $(".tweets-container").html("");
+    containerEffect();
+    loadTweets();
+  });
+  containerEffect();
+  loadTweets();
+  // renderTweets(tweetData);
 });
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -6,13 +6,15 @@ const simulateDelay = require("./util/simulate-delay");
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
+        console.log("newTweet: ", newTweet);
         db.tweets.push(newTweet);
         callback(null, true);
+        console.log("db after push: ", db);
       });
+      console.log("saveTweet db: ", db);
     },
 
     // Get all tweets in `db`, sorted by newest first
@@ -22,6 +24,5 @@ module.exports = function makeDataHelpers(db) {
         callback(null, db.tweets.sort(sortNewestFirst));
       });
     }
-
   };
-}
+};
